1c63d0c48848cbb6602276358f0b6495b2a7da28
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-08-06 11:32:32 +0200

    Revert "Revert "Revert "Delete const.php"""
    This reverts commit 71ac52b5386fa7092ec832bf0608d8166571af00.

---
+++ const.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fichier contenant les variables globales définissant l'environnement du blog
+ */
+
+$GLOBALS = [
+    'url' => 'http://localhost/P4/index.php',
+    'secretKey' => '6LdYUlwUAAAAAIo8RnwqIR_tJ2aW2zNKI2jTxnqi',
+    'siteKey' => '6LdYUlwUAAAAAAC5xhS_Ta72loK4qTWf7ESVgpzJ',
+    'host' => 'localhost',
+    'dbname' => 'p4',
+    'charset' => 'utf8',
+    'user' => 'root',
+    'password' => ''
+];
