35ad72cce052874214864a4f6c6f5d71adcbfa0b
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-08-10 14:19:33 +0200

    Amélioration oral

--- model/Manager.php
+++ model/Manager.php
@@ -3,14 +3,21 @@
 namespace P4\model;
 
 /**
- * Classe mère des autres manangers.
- * Elle permet la connexion à la base de données en prenant les valeurs définies dans le fichier const.php
+ * Classe mère des autres Managers.
+ * Elle permet la connexion à la base de données
+ * en prenant les valeurs définies dans le fichier const.php
  */
 class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=' . $GLOBALS['host'] . ';dbname=' . $GLOBALS['dbname'] . ';charset=' . $GLOBALS['charset'], $GLOBALS['user'], $GLOBALS['password']);
+        $db = new \PDO('mysql:
+        host=' . $GLOBALS['host'] . ';
+        dbname=' . $GLOBALS['dbname'] . ';
+        charset=' . $GLOBALS['charset'],
+        $GLOBALS['user'],
+        $GLOBALS['password']
+        );
         return $db;
     }
 }
